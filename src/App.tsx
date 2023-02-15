0075dc6403d93c6d582669d2e6143dd30b6ea39d
BYeungCoding <bluefroggy321@icloud.com>
2023-02-15 18:06:17 -0500

    html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
+            <h1> This is my Header</h1>
+            <img src="./Images/Eleceed" alt="A picture of my favorite manhwa" />
+            <ol>
+                <li> Eleceed </li>
+                <li> Talent-Swallowing Magician</li>
+                <li> I Obtained a Mythic Item</li>
+            </ol>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <header className="App-header">
                 Brandon Yeung UD CISC275 with React Hooks and TypeScript, Hello
                 World
@@ -12,6 +23,16 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="rectangle"></div>Action
+                    </Col>
+                    <Col>
+                        <div className="rectangle"></div>Romance
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
